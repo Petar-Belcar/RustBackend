f74e7eb9f5306ebc662ed3ea48f862cf8fac9c0e
Petar-Belcar <petar.belcar@gmail.com>
2023-07-03 19:10:21 +0200

    Changed row arithmetic to return a result

--- src/row_arithmetic/mod.rs
+++ src/row_arithmetic/mod.rs
@@ -1,15 +1,15 @@
-pub fn subtract_rows_setting_column_to_zero<'a>(subtrahend: &'a mut [f32], minuend: &'a [f32], column: u32)
+pub fn subtract_rows_setting_column_to_zero<'a>(subtrahend: &'a mut [f32], minuend: &'a [f32], column: u32) -> Result<bool, String>
 {
     if subtrahend.len() != minuend.len()
     {
-        panic!("Rows must be of the same length: [subtrahend = {}, minuend = {}]", subtrahend.len(), minuend.len());
+        return Err(format!("Rows must be of the same length: [subtrahend = {}, minuend = {}]", subtrahend.len(), minuend.len()))
     }
 
     let subtrahend_len: u32 = subtrahend.len().try_into().unwrap();
 
     if column >= subtrahend_len - 1
     {
-        panic!("The column which is to be set to 0 cannot be the last column or outside of the length of the subtrahend: [column = {}, subtrahend = {}]", column, subtrahend_len);
+        return Err(format!("The column which is to be set to 0 cannot be the last column or outside of the length of the subtrahend: [column = {}, subtrahend = {}]", column, subtrahend_len))
     }
 
     let multiplier = determine_how_much_to_multiply_by(subtrahend, minuend, column);
@@ -20,6 +20,8 @@ pub fn subtract_rows_setting_column_to_zero<'a>(subtrahend: &'a mut [f32], minue
         *subtrahend_column = *subtrahend_column + multiplier * minuend[minuend_column as usize];
         minuend_column += 1;
     }
+
+    Ok(true)
 }
 
 fn determine_how_much_to_multiply_by<'a>(subtrahend: &'a mut [f32], minuend: &'a [f32], column: u32) -> f32
--- src/row_arithmetic_test.rs
+++ src/row_arithmetic_test.rs
@@ -9,7 +9,11 @@ mod tests {
         let minuend: [f32; 4] = [1.0, 0.0, 0.0, 0.0];
         let column = 0;
 
-        row_arithmetic::subtract_rows_setting_column_to_zero(&mut subtrahend, &minuend, column);
+        let arithmetic_result = match row_arithmetic::subtract_rows_setting_column_to_zero(&mut subtrahend, &minuend, column)
+        {
+            Ok(_) => true,
+            Err(_) => false
+        };
 
         let mut all_elements_zero = true;
         for element in subtrahend
@@ -20,51 +24,39 @@ mod tests {
             }
         }
 
-        assert!(all_elements_zero);
+        assert!(all_elements_zero && arithmetic_result);
     }
 
     #[test]
-    #[should_panic]
     fn column_too_large() // Set tests to expect to panic - although maybe we should panic somewhere else
     {
         let mut subtrahend: [f32; 4] = [1.0, 0.0, 0.0, 0.0];
         let minuend: [f32; 4] = [1.0, 0.0, 0.0, 0.0];
         let column = 4;
 
-        row_arithmetic::subtract_rows_setting_column_to_zero(&mut subtrahend, &minuend, column);
-
-        let mut all_elements_zero = true;
-        for element in subtrahend
+        let arithmetic_result = match row_arithmetic::subtract_rows_setting_column_to_zero(&mut subtrahend, &minuend, column)
         {
-            if element != 0.0
-            {
-                all_elements_zero = false;
-            }
-        }
+            Ok(_) => true,
+            Err(_) => false
+        };
 
-        assert!(all_elements_zero);
+        assert!(!arithmetic_result);
     }
 
     #[test]
-    #[should_panic]
     fn arrays_not_same_size()
     {
         let mut subtrahend: [f32; 4] = [1.0, 0.0, 0.0, 0.0];
         let minuend: [f32; 3] = [1.0, 0.0, 0.0];
         let column = 0;
 
-        row_arithmetic::subtract_rows_setting_column_to_zero(&mut subtrahend, &minuend, column);
-
-        let mut all_elements_zero = true;
-        for element in subtrahend
+        let arithmetic_result = match row_arithmetic::subtract_rows_setting_column_to_zero(&mut subtrahend, &minuend, column)
         {
-            if element != 0.0
-            {
-                all_elements_zero = false;
-            }
-        }
+            Ok(_) => true,
+            Err(_) => false
+        };
 
-        assert!(all_elements_zero);
+        assert!(!arithmetic_result);
     }
 
 }
