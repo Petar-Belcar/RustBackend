d78abcab96eeeea0f3d4c264c34973c1915d2cf1
Petar-Belcar <petar.belcar@gmail.com>
2023-10-04 09:30:59 +0200

    Added logic to solve a linear system of equations

--- src/solve_linear_equations/mod.rs
+++ src/solve_linear_equations/mod.rs
@@ -14,7 +14,6 @@ pub struct LinearSystem
 pub enum RowColumnResult
 {
     Found((usize, usize)),
-    NoSolution,
     NotFound
 }
 
@@ -25,26 +24,43 @@ impl LinearSystem
         LinearSystem {equations, solution: Some(HashMap::new())}
     }
 
-    pub fn solve_system_of_linear_equations(&self) -> Vec<f32>
+    // Assume here that the number of equations is lesser than or equal to the number of variables
+    pub fn solve_system_of_linear_equations(&mut self) -> Result<bool, String>
     {
-        todo!();
+        let mut solution_map: HashMap<(usize, usize), f32> = HashMap::new();
+        let mut iteration_count: usize = 0;
+
+        while solution_map.len() != self.equations.len() && iteration_count < self.equations[0].a_ij.len()
+        {
+            iteration_count += 1;
+            match self.determine_column_and_row(&solution_map)
+            {
+                RowColumnResult::Found((row, column)) =>
+                {
+                    match self.reduce_rows(column, row)
+                    {
+                        Ok(_) => (),
+                        Err(error) => return Err(error)
+                    };
+
+                    solution_map.insert((row, column), self.equations[row].b_i);
+                },
+                RowColumnResult::NotFound => return Err(format!("Linear set of equations does not have enough")),
+
+            }
+        }
+
+        Ok(true)
     }
 
-    pub fn determine_column_and_row(&self) -> RowColumnResult
+    pub fn determine_column_and_row(&self, solution_map: &HashMap<(usize, usize), f32>) -> RowColumnResult
     {
-        let solution_set = match &self.solution
-        {
-            Some(set) => set,
-            None => return RowColumnResult::NoSolution
-        };
-
-
         for (row_index, row) in self.equations.iter().enumerate()
-            .filter(|(index, _)| !solution_set.iter().any(|(key, _)| key.0 == *index))
+            .filter(|(index, _)| !solution_map.iter().any(|(key, _)| key.0 == *index))
         {
             match row.a_ij.iter().enumerate()
                 .filter(|(index, value)| 
-                    !solution_set.iter().any(|(key, _)| key.1 == *index) && **value > 0.0
+                    !solution_map.iter().any(|(key, _)| key.1 == *index) && **value != 0.0
                 ).nth(0)
             {
                 None => (),
@@ -53,5 +69,28 @@ impl LinearSystem
         }
 
         RowColumnResult::NotFound
+    }
+
+    fn reduce_rows(&mut self, column: usize, row: usize) -> Result<bool, String>
+    {
+        match self.equations[row].reduce_row_till_column_one(column)
+        {
+            Ok(_) => (),
+            Err(error) => return Err(error),
+        };
+
+        let equation_to_reduce_by = self.equations[row].clone();
+
+        for equation in self.equations.iter_mut().enumerate()
+            .filter(|(index, _)| *index != row).map(|(_, row)| row)
+        {
+            match equation.reduce_row(&equation_to_reduce_by, column)
+            {
+                Ok(_) => (),
+                Err(error) => return Err(error)
+            };
+        }
+
+        Ok(true)
     }
 }
